341a602d6938bbf94eff1ab43a4c15ca4405bd06
MohammedYaseenRon <yaseenron3@gmail.com>
2026-04-20 15:48:33 +0530

    cors update

--- server/src/index.ts
+++ server/src/index.ts
@@ -16,7 +16,7 @@ app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", "https://talent-arena-iota.vercel.app"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
